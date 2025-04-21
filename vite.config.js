7104d374c93e1fbd0648d23442dcf68187112a8f
suzyobaii <kellyyjjo@gmail.com>
2025-04-21 01:15:55 -0400

    retry

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+//export default defineConfig({
+  //base: '/REACT/',
+  //plugins: [react()],
+//})
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/REACT/',
-  plugins: [react()],
-})
+  base: '/REACT/',  
+});
